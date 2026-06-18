b7394955213fd43818c57a415a24835236478529
github_Rohit73108 <rohitrana73108@gmail.com>
2026-06-18 19:10:56 +0530

    data types

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// what is language?:language is the medium of communication
+// what is the token: tokens are the smallest unit of any programing language;
+// keywaords:keywords are predfined words 
+// variables:variables are the container which used to store some value and variables are changable
+// constants:contants are the container but we cant chnage the value of the constants
+
+// let a=10;
+// let b=20;
+// var result=a+b;
+// console.log(result);
+// console.log(typeof(a));
+
+// // data types:
+// // number,String,boolean,undefined,null,BigInt,Symbol;
+// let name;
+// let d=null
+// console.log(name);
+// console.log(d);
+
+// let a=4.680
+// console.log(a)
+// let number1=4573987545342342657;
+// let number2=324
+// console.log(BigInt(number1*number2));
+let string="23";
+let num=Number(string)
+console.log(typeof(num));
+console.log(10+string);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
